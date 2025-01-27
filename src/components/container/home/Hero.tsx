73e2a66b13a7160570541251aa7259301fbb7ac7
Xebec <manueljosepera487@gmail.com>
2025-01-27 10:46:57 +0800

    merge upadates

--- src/components/container/home/Hero.tsx
+++ src/components/container/home/Hero.tsx
@@ -1,20 +1,26 @@
 import React from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import HeartRateGraph from "./HertRateGraph";
 
 const Hero = () => {
+  // Sample heart rate data
+  const heartRateData = [
+    58, 58, 80, 20, 100, 60, 90, 30, 90, 50, 100, 80, 100, 40, 60, 60,
+  ];
   return (
     <div className="h-screen">
-      <div className="bg-red-700 h-2/3 flex flex-col">
+      <div className="bg-main h-2/3 flex flex-col">
         {/* Navbar */}
         <nav className="flex justify-between items-center p-5">
           <div className="flex items-center">
             <Image
-              src="/Outlook-gig2o1qk.png"
+              src="/logoo.png"
               alt="Logo"
-              width={72}
-              height={72}
-              className="mr-2"
+              width={62}
+              height={62}
+              className="mr-2 w-auto h-auto"
             />
             <span className="text-xl font-extralight text-white">
               Java Condition Monitoring
@@ -27,12 +33,14 @@ const Hero = () => {
             >
               Home
             </Button>
-            <Button
-              variant="default"
-              className="bg-transparent shadow-none text-white text-xl font-semibold hover:bg-red-900"
-            >
-              Login
-            </Button>
+            <Link href={"/auth/sign-in"}>
+              <Button
+                variant="default"
+                className="bg-transparent shadow-none text-white text-xl font-semibold hover:bg-red-900"
+              >
+                Login
+              </Button>
+            </Link>
           </div>
         </nav>
 
@@ -42,6 +50,10 @@ const Hero = () => {
             Machinery Health Specialist
           </h1>
         </div>
+      </div>
+      <div className="absolute bottom-5 left-0 right-0">
+        {/* HeartRateGraph Component with overflow */}
+        <HeartRateGraph data={heartRateData} />
       </div>
     </div>
   );
