c2e1f3e7f944acbbd0db63413cc73804e07e6d69
Uriah08 <lorenz08.flores@gmail.com>
2025-01-31 05:20:48 +0800

    Authentication Added

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,4 +1,23 @@
 import { z } from "zod";
+import { prisma } from '@/lib/db'
+
+export const findUserById = async (id: string) => {
+  const user = prisma.user.findUnique({
+    where: {
+      id
+    }
+  })
+  return user
+}
+
+export const getUserByEmail = async (email: string) => {
+  const user = prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+  return user
+}
 
 export const jobSchema = z.object({
   client: z.string().min(1, { message: "Client is required" }),
@@ -20,3 +39,26 @@ export const jobSchema = z.object({
   }),
   yearWeekNo: z.string().min(1, { message: "Year week number is required" }),
 });
+
+export const registerSchema = z.object({
+  username: z.string().min(5,{
+    message: "Username must be at least 5 characters long",
+  }).max(30,{
+    message: "Username must be at most 30 characters long",
+  }),
+  email: z.string().email().min(5).max(50),
+  password: z.string().min(8,{
+    message: "Password must be at least 8 characters long",
+  }).max(20,{
+    message: "Password must be at most 20 characters long",
+  }),
+})
+
+export const loginSchema = z.object({
+  email: z.string().email().min(2).max(50),
+  password: z.string().min(8,{
+    message: "Password must be at least 8 characters long",
+  }).max(20,{
+    message: "Password must be at most 20 characters long",
+  }),
+})
