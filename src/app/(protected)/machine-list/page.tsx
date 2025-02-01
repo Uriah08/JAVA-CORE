72aa566e7bcc2f4a00327ea892f4fb1f81dab4b2
Xebec <manueljosepera487@gmail.com>
2025-02-01 19:11:31 +0800

    prisma update

---
+++ src/app/(protected)/machine-list/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full p-5 flex pr-8">
+      <div className="w-full h-full p-5 bg-white rounded-xl ">
+        <h1 className="text-2xl font-bold">Machine list</h1>
+      </div>
+    </div>
+  );
+};
+
+export default page;
