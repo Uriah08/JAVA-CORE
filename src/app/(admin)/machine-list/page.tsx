9ba737ffaac05021a89b696da8bf53bbdf334f44
Uriah08 <lorenz08.flores@gmail.com>
2025-02-09 10:31:18 +0800

    Local Storage

--- src/app/(admin)/machine-list/page.tsx
+++ src/app/(admin)/machine-list/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full h-screen p-5 flex md:flex-row flex-col pr-8 gap-5">
+    <div className="w-full h-screen p-5 flex md:flex-row flex-col gap-5">
       <div className="w-2/3 min-h-[90vh] p-5 bg-white rounded-xl shadow-lg">
         <h1 className="text-2xl font-bold">Machine list</h1>
         <List />
